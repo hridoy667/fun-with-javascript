73e89d979a6e4225bac33fcad3b494ebddfb0f98
Ali Imam Hridoy <hridoy15-14667@diu.edu.bd>
2025-05-15 20:12:56 +0600

    two code added

---
+++ project.js
@@ -0,0 +1,21 @@
+
+function addition(a,b){
+    return a+b;
+}
+var result=addition(10,20)
+console.log(result);
+
+
+// even odd checker
+
+function isEven(num){
+    if (num % 2 == 0) {
+        console.log('This is an Even number');
+        
+    }
+    else {
+        console.log('This is an Odd number');
+        }
+}
+
+isEven(5);
